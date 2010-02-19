0ea9a938cd3c92663f3b67e668cd3c8f856773a7
Adrià Casajús <adria@ecm.ub.es>
2010-02-19 15:33:54 +0000

    Migrating dirac-make scripts to python
    git-svn-id: http://svn.cern.ch/guest/dirac/Externals/trunk/pyGSI@21908 4525493e-7705-40b1-a816-d608a930855b

---
+++ dirac-make.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import imp, os, sys, platform
+
+here = os.path.dirname( os.path.abspath( __file__ ) )
+chFilePath = os.path.join( os.path.dirname( here ) , "common", "CompileHelper.py" )
+try:
+  fd = open( chFilePath )
+except Exception, e:
+  print "Cannot open %s: %s" % ( chFilePath, e )
+  sys.exit( 1 )
+
+chModule = imp.load_module( "CompileHelper", fd, chFilePath, ( ".py", "r", imp.PY_SOURCE ) )
+fd.close()
+chClass = getattr( chModule, "CompileHelper" )
+
+compileOpenSSL = True
+
+if compileOpenSSL:
+  osslch = chClass( os.path.join( here, "openssl" ) )
+
+  versions = { 'openssl' : "0.9.7m" }
+  osslch.setPackageVersions( versions )
+
+  osslch.unTarPackage( "openssl" )
+  ret = osslch.execRawAndGetOutput( "gcc -dumpversion" )
+
+  if not osslch.doConfigure( "openssl", extraArgs = "shared threads", configureExecutable = "config" ):
+    osslch.ERROR( "Could not deploy openssl package" )
+    sys.exit( 1 )
+
+  if ret:
+    if ret[0].strip() >= "4.3.0":
+      makefilePath = os.path.join( osslch.getPackageDir( 'openssl' ), 'Makefile' )
+      osslch.INFO( "Patching %s" % makefilePath )
+      osslch.replaceInFile( makefilePath, "-m486", "-mtune=i486" )
+
+  if not osslch.doMake( "openssl", makeJobs = 1 ):
+    osslch.ERROR( "Could not deploy openssl package" )
+    sys.exit( 1 )
+
+ch = chClass( here )
+
+for step in ( "build", "bdist_egg" ):
+  if not ch.pythonExec( "setup.py", extraArgs = step ):
+    ch.error( "Could not deploy GSI" )
+    sys.exit( 1 )
+
+if not ch.easyInstall( here ):
+  ch.error( "Could not deploy GSI" )
+  sys.exit( 1 )
